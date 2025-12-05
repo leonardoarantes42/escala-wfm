4f4a765fe3c571ba84e3a343ec96d84b60fd8251
leonardoarantes42 <leonardo.arantes@turbi.com.br>
2025-12-05 16:18:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -41,12 +41,12 @@ st.markdown("""
 
         /* Altura específica para VISÃO MENSAL */
         .height-mensal {
-            height: calc(100vh - 280px); 
+            height: calc(100vh - 290px); 
         }
 
         /* Altura específica para VISÃO DIÁRIA */
         .height-diaria {
-            height: calc(100vh - 300px); 
+            height: calc(100vh - 310px); 
         }
         
         /* Altura específica para ADERÊNCIA (Nova) */
