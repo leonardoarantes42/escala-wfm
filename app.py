23228c3b8ed1aab9acab4cfbb8317a56cd2e2dc6
leonardoarantes42 <leonardo.arantes@turbi.com.br>
2025-12-16 17:07:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ def renderizar_tabela_html(df, modo_cores='diario', classe_altura='height-diaria
         val_str = str(val).upper().strip()
         
         # 1. DIVISORES VISUAIS (Adicionei PLENO e STAFF aqui)
-        if val_str in ['FINANCEIRO', 'E-MAIL', 'FINANCEIRO ASSINCRONO', 'ANALISTAS JR TRABALHANDO NO DIA', 'PLENO', 'STAFF']:
+        if val_str in ['FINANCEIRO', 'E-MAIL', 'FINANCEIRO ASSINCRONO', 'PLENO', 'STAFF']:
             return 'background-color: #000000; color: white; font-weight: bold;'
             
         # 2. REGRA GERAL (Vale para Mensal e Diário)
@@ -301,8 +301,9 @@ def renderizar_tabela_html(df, modo_cores='diario', classe_altura='height-diaria
             
         else: # Modo Diário
             if val_str == 'F': return 'background-color: #002060; color: white'
-            elif val_str == 'AF': return 'background-color: #f4cccc; color: black' # Igual ao Mensal
-            elif val_str == 'RT': return 'background-color: #e6cff2; color: black' # Lilás solicitado
+            elif val_str == 'AF': return 'background-color: #f4cccc; color: black' 
+            elif val_str == 'RT': return 'background-color: #e6cff2; color: black'
+            elif val_str == 'E-mail': return 'background-color: #bfe1f6; color: black'    
             
             elif 'CHAT' in val_str: return 'background-color: #d9ead3; color: black'
             elif 'PAUSA' in val_str or val_str == 'P': return 'background-color: #fce5cd; color: black'
