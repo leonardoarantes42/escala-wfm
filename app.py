bb96e5c9d36990520b522cd6d82e07fdec660701
leonardoarantes42 <leonardo.arantes@turbi.com.br>
2025-12-18 15:15:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -850,7 +850,7 @@ if eh_admin and aba_aderencia:
                  fig_b = px.bar(df_ad, x='Data', y=['Realizado (T)', 'Afastado (AF)', 'Turnover (TO)'], text_auto='.0f', title="Evolução de Presença", color_discrete_map={'Realizado (T)': '#1e3a8a', 'Afastado (AF)': '#d32f2f', 'Turnover (TO)': '#000000'})
                  
                  fig_b.update_layout(
-                     height=300, 
+                     height=400, 
                      margin=dict(t=30, b=0, l=0, r=0), 
                      showlegend=True,
                      xaxis_title=None,
@@ -879,7 +879,7 @@ if eh_admin and aba_aderencia:
                     fig_l.update_xaxes(type='category', tickangle=30)
                     
                     fig_l.update_layout(
-                        height=300, 
+                        height=400, 
                         margin=dict(t=30, b=0, l=0, r=0),
                         xaxis_title=None,
                         yaxis_title="Total (menos e-mail e Projeto)",
